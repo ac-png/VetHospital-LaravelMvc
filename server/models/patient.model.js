f1ff1d1133ffa9c1ca095a42b6e71934b93d46fc
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 12:50:09 +0000

    Patient Model

---
+++ server/models/patient.model.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const patientSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    species: {
+        type: String,
+        required: [true, 'Species field is required']
+    },
+    breed: {
+        type: String,
+        required: [true, 'Breed field is required']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required']
+    },
+    image: {
+        type: String,
+    }
+}, { timestamps: true});
+
+module.exports = model('Patient', patientSchema);
