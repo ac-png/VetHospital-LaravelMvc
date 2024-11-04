862aac6c43e2b1e01835a2fd18526f93c248579e
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 13:51:59 +0000

    Adding patient routes to server file

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ require('./config/db.js')();
 app.use(express.json());
 app.use(express.static(__dirname + '/views/'));
 
+app.use('/api/patients', require('./routes/patients'))
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
