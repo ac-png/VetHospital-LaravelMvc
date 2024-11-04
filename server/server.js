620670ba636b44b39aa77454536ca42fa690f7d6
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 13:33:01 +0000

    Adding views to server file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const app = express();
 const port = 5001;
 
 require('dotenv').config();
 require('./config/db.js')();
+
+app.use(express.json());
+app.use(express.static(__dirname + '/views/'));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
