ca25f9dacc039cbd76d56afcc43ed89934eff140
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 15:43:57 +0000

    Changing file name

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -1,4 +1,4 @@
-const Patient = require('../models/patient.model');
+const Patient = require('../models/patients.model');
 
 const readAll = (req, res) => {
 };
@@ -7,6 +7,27 @@ const readOne = (req, res) => {
 };
 
 const createData = (req, res) => {
+    console.log(req.body);
+    let body = req.body;
+
+    Patient.create(body)
+        .them(data => {
+            console.log(`New patient created`, data);
+
+            return res.status(201).json({
+                message: `Patient created`,
+                data
+            });
+        })
+        .catch(err => {
+            console.log(err);
+
+            if(err.name === 'ValidationError'){
+                return res.status(422).json(err)
+            }
+
+            return res.status(500).json(err);
+        });
 };
 
 const updateData = (req, res) => {
