637a73865b32c4f61f8f249d4d13bf9cfd71f41d
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 13:14:53 +0000

    Connecting to Database

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const init = () => {
+    mongoose.set('debug', true);
+
+    mongoose.connect(process.env.DB_ATLAS_URL)
+    .catch(err => {
+        console.log(`Error: ${err.stack}`);
+    });
+
+    mongoose.connection.on('open', () => {
+        console.log('Connected to Database');
+    });
+};
+
+module.exports = init;
