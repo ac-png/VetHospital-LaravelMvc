8302ce293b00ba4c9eb2aafe34615963a3c04ad3
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 15:57:02 +0000

    Adding rest of seeders to seed file

--- server/seed.js
+++ server/seed.js
@@ -8,15 +8,30 @@ const Patient = require('./models/patients.model.js');
 const seedAppointments = require('./seeds/appointments');
 const Appointments = require('./models/appointments.model');
 
+const seedBilling = require('./seeds/billing');
+const Billing = require('./models/billing.model');
+
+const seedTreatments = require('./seeds/treatments');
+const Treatments = require('./models/treatments.model');
+
+const seedMedications = require('./seeds/medications');
+const Medications = require('./models/medications.model');
+
 const clearDatabase = async () => {
     await Patient.deleteMany({});
     await Appointments.deleteMany({});
+    await Billing.deleteMany({});
+    await Treatments.deleteMany({});
+    await Medications.deleteMany({});
 };
 
 const seedDatabase = async () => {
     await clearDatabase();
     await seedPatients(10);
     await seedAppointments(10);
+    await seedBilling(10);
+    await seedTreatments(10);
+    await seedMedications(10);
 };
 
 module.exports = seedDatabase;
