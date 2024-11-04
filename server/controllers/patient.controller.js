fd22223adbe5ee051df7618c1e0046db9b274b00
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 13:32:26 +0000

    Creating layout of patient controller

---
+++ server/controllers/patient.controller.js
@@ -0,0 +1,25 @@
+const Patient = require('../models/patient.model');
+
+const readAll = (req, res) => {
+};
+
+const readOne = (req, res) => {
+};
+
+const createData = (req, res) => {
+};
+
+const updateData = (req, res) => {
+
+};
+
+const deleteData = (req, res) => {
+};
+
+module.exports = {
+    readAll,
+    readOne,
+    createData,
+    updateData,
+    deleteData
+};
