e1b2412c36f870a599ccf6f312c2cb2180f3f88d
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 13:15:34 +0000

    Making appointments model

---
+++ server/models/appointments.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const appointmentSchema = new Schema({
+    patient: {
+        type: Schema.Types.ObjectId,
+        ref: 'Patient',
+        required: [true, 'Patient is required']
+    },
+    appointment_date: {
+        type: Date,
+        required: [true, 'Date field is required']
+    },
+    status: {
+        type: String,
+        required: [true, 'Status field is required']
+    },
+    notes: {
+        type: String,
+    }
+}, { timestamps: true });
+
+module.exports = model('Appointment', appointmentSchema);
