80a74471d0ef7f8b93b0fcc587a851c723ad7194
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-04 14:00:12 +0000

    Adding more route files

---
+++ server/routes/appointments.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const { 
+    readAll, 
+    readOne,
+    createData,
+    updateData,
+    deleteData
+} = require('../controllers/appointment.controller');
+
+router.get('/', readAll);
+router.get('/:id', readOne);
+router.post('/', createData);
+router.put('/:id', updateData);
+router.delete('/:id', deleteData);
+
+module.exports = router;
