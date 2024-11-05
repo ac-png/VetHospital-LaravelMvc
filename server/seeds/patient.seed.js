3de04e1762c39ae25bf4dab376d4b1ae5b69ff40
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-05 13:47:13 +0000

    Adding my own list of species and then using them for the breeds

--- server/seeds/patient.seed.js
+++ server/seeds/patient.seed.js
@@ -1,15 +1,18 @@
 const { faker } = require('@faker-js/faker');
 const Patient = require('../models/patient.model');
 
+const speciesList = ['Dog', 'Cat', 'Horse', 'Rabbit', 'Bird', 'Fish'];
+
 const seedPatients = async (num) => {
     const patients = [];
     for (let i = 0; i < num; i++) {
+        const species = faker.helpers.arrayElement(speciesList);
         patients.push({
             name: faker.person.firstName(),
-            species: faker.word.adjective(),
-            breed: faker.word.adjective(),
-            age: faker.number.int(15),
-            owner: faker.person.firstName()
+            species: species,
+            breed: getBreedBySpecies(species),
+            age: faker.number.int({ min: 1, max: 15 }),
+            owner: faker.person.fullName()
         });
     }
 
@@ -18,6 +21,25 @@ const seedPatients = async (num) => {
         console.log(`${num} patients seeded successfully!`);
     } catch (error) {
         console.error('Error seeding patients:', error);
+    }
+};
+
+const getBreedBySpecies = (species) => {
+    switch (species) {
+        case 'Dog':
+            return faker.animal.dog();
+        case 'Cat':
+            return faker.animal.cat();
+        case 'Horse':
+            return faker.animal.horse();
+        case 'Rabbit':
+            return faker.animal.rabbit();
+        case 'Bird':
+            return faker.animal.bird();
+        case 'Fish':
+            return faker.animal.fish();
+        default:
+            return 'Unknown Breed';
     }
 };
 
