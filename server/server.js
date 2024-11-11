e33e5fd5ba6f8e2c6c3bad47a3bcbd6921f2255d
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-11 14:08:21 +0000

    Adding Authorisation

--- server/server.js
+++ server/server.js
@@ -8,6 +8,26 @@ require('./config/db.js')();
 app.use(express.json());
 app.use(express.static(__dirname + '/views/'));
 
+//// AUTHORIZATION /////////////
+app.use((req, res, next) => {
+    let authHeader = req.headers?.authorization?.split(' ');
+
+    if(req.headers?.authorization && authHeader[0] === 'Bearer'){
+        jwt.verify(authHeader[1], process.env.JWT_SECRET, (err, decoded) => {
+            if(err) req.user = undefined;
+            req.user = decoded;
+            next();
+        });
+    }
+    else {
+        req.user = undefined;
+        next();
+    }
+});
+//////////////////////////////
+
+app.use('/api/users', require('./routes/users.route.js'))
+app.use('/api/roles', require('./routes/roles.route.js'))
 app.use('/api/appointments', require('./routes/appointments.route.js'))
 app.use('/api/billing', require('./routes/billings.route.js'))
 app.use('/api/patients', require('./routes/patients.route.js'))
