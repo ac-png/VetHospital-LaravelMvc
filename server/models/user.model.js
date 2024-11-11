50740999b8402ba92c52c9c4b38d76a5ceea1e8c
Alice Corry <82654782+ac-png@users.noreply.github.com>
2024-11-11 23:46:05 +0000

    Adding email validation and compare password

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,17 +1,29 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const validateEmail = (email) => {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(email);
+};
 
 const userSchema = new Schema({
     username: {
         type: String,
-        required: [true, 'Username field is required']
+        required: true,
+        trim: true
     },
     password: {
         type: String,
-        required: [true, 'Password field is required']
+        required: true
     },
     email: {
         type: String,
-        required: [true, 'Email field is required']
+        unique: true,
+        lowercase: true,
+        trim: true,
+        required: true,
+        // validate: [validateEmail, 'Please use a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address']
     },
     role: {
         type: Schema.Types.ObjectId,
@@ -19,5 +31,11 @@ const userSchema = new Schema({
         required: [true, 'Role is required']
     }
 }, { timestamps: true});
+
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password, function(result){
+        return result
+    });
+};
 
 module.exports = model('User', userSchema);
