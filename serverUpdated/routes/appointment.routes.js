9dee2a7c899e84c8647c174f6e6c8a0a05e42417
Alice Corry <82654782+ac-png@users.noreply.github.com>
2025-01-01 14:34:30 +0000

    Editing role names in routes files

--- serverUpdated/routes/appointment.routes.js
+++ serverUpdated/routes/appointment.routes.js
@@ -9,9 +9,9 @@ const {
 } = require('../controllers/appointment.controller');
 const router = express.Router();
 
-router.post('/', authenticate, authorize(['admin']), createAppointment);
-router.delete('/:id', authenticate, authorize(['admin']), deleteAppointment);
-router.put('/:id', authenticate, authorize(['admin']), updateAppointment);
+router.post('/', authenticate, authorize(['veterinarian', 'admin']), createAppointment);
+router.delete('/:id', authenticate, authorize(['veterinarian', 'admin']), deleteAppointment);
+router.put('/:id', authenticate, authorize(['veterinarian', 'admin']), updateAppointment);
 router.get('/', authenticate, getAllAppointments);
 router.get('/:id', authenticate, getAppointmentById);
 
--- serverUpdated/routes/auth.routes.js
+++ serverUpdated/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { register, login } = require('../controllers/auth.controller');
-const { authenticate, authorize } = require('../middleware/auth.middleware');
+const { authenticate } = require('../middleware/auth.middleware');
 
 const router = express.Router();
 
@@ -9,10 +9,6 @@ router.post('/login', login);
 
 router.get('/profile', authenticate, (req, res) => {
   res.json(req.user);
-});
-
-router.get('/admin', authenticate, authorize(['admin']), (req, res) => {
-  res.json({ message: 'Welcome, Admin!' });
 });
 
 module.exports = router;
