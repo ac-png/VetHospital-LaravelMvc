fedf3d031e5554d250215d21e4036cca13ee204a
Alice Corry <82654782+ac-png@users.noreply.github.com>
2025-01-03 11:31:14 +0000

    Adding all appointments page, and styling with React Native Paper

--- client/app/(tabs)/appointments/index.tsx
+++ client/app/(tabs)/appointments/index.tsx
@@ -1,26 +1,124 @@
-import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import { Text, StyleSheet, FlatList, View } from 'react-native';
 import { useSession } from '@/contexts/AuthContext';
+import useAPI from '@/hooks/useAPI';
+import { Link } from 'expo-router';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
-export default function Tab() {
+export default function AppointmentsPage() {
     const { session } = useSession();
-    
+    const [appointments, setAppointments] = useState([]);
+
+    const { getRequest, loading, error } = useAPI();
+
+    useEffect(() => {
+        if (!session) {
+            return;
+        }
+
+        getRequest('http://localhost:5001/api/appointments', {
+            headers: {
+                Authorization: `Bearer ${session}`,
+            },
+        }, (data) => {
+            setAppointments(data);
+        });
+    }, [session]);
+
+    console.log(session)
+
+    if (loading) return <Text>Loading appointments...</Text>;
+
+    if (error) return <Text style={styles.errorText}>Failed to load appointments: {error}</Text>;
+
     return (
-        <View style={{ flex: 1 }}>            
-            <View style={{ alignItems: 'center', paddingHorizontal: 20 }}>
-                <Text variant="headlineSmall" style={{ margin: 20 }}>
-                    Appointments
-                </Text>
-                {session ? (
-                    <Text>
-                        Welcome to the appointments page!
-                    </Text>
-                ) : (
-                    <Text variant="bodyLarge" style={{ marginTop: 20 }}>
-                        Please log in to access your appointments.
-                    </Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Appointments</Text>
+
+            <FlatList
+                data={appointments}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => (
+                    <Card style={styles.card}>
+                        <Card.Content>
+                            <Title>{item.reason}</Title>
+                            <Paragraph style={styles.dateText}>
+                                {new Date(item.date).toLocaleString()}
+                            </Paragraph>
+                            <Paragraph style={styles.statusText}>
+                                <Text>Status: </Text>
+                                <Text style={styles.status}>{item.status}</Text>
+                            </Paragraph>
+                        </Card.Content>
+                        <Card.Actions>
+                        <Link href={{
+                                pathname: '/appointments/[id]',
+                                params: { id: item._id }
+                            }}><Text>View Details</Text></Link>
+                        </Card.Actions>
+                    </Card>
                 )}
-            </View>
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#f4f4f4',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    createButton: {
+        marginBottom: 20,
+        marginTop: 10,
+        backgroundColor: '#01ff00',
+    },
+    progressBar: {
+        marginTop: 20,
+    },
+    card: {
+        marginBottom: 15,
+        borderRadius: 8,
+        backgroundColor: '#fff',
+        elevation: 4,
+        shadowColor: 'rgba(0, 0, 0, 0.1)',
+        shadowOffset: { width: 0, height: 1 },
+        shadowRadius: 5,
+        shadowOpacity: 0.1,
+    },
+    dateText: {
+        fontSize: 14,
+        color: '#888',
+        marginVertical: 5,
+    },
+    statusText: {
+        fontSize: 14,
+        color: '#888',
+        marginVertical: 5,
+    },
+    status: {
+        fontWeight: 'bold',
+        color: '#00796b',
+    },
+    button: {
+        marginTop: 10,
+    },
+    errorText: {
+        color: 'red',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    noAppointmentsText: {
+        textAlign: 'center',
+        color: '#888',
+        fontSize: 16,
+        marginTop: 20,
+    },
+});
--- client/types/index.d.ts
+++ client/types/index.d.ts
@@ -1,29 +1,18 @@
-export interface PatientType {
-    _id?: string;
-    name: string;
-    age: number;
-    type: string;
+export interface AppointmentType {
+    date: string;
+    reason: string;
+    status: string;
+    notes: string;
+    user: string;
+    patient: string;
+    veterinarian: string;
+    hospital: string;
 }
-export interface PatientTypeID extends PatientType {
+
+export interface AppointmentTypeID extends AppointmentType {
     _id: string;
 }
-export interface HospitalType {
-    _id?: string;
-    name: string;
-    address: string;
-}
-export interface HospitalTypeID extends HospitalType {
-    _id: string;
-}
-export interface VeterinarianType {
-    _id?: string;
-    name: string;
-    address: string;
-    bio: string;
-}
-export interface VeterinarianTypeID extends VeterinarianType {
-    _id: string;
-}
+
 export interface IAuthContext {
     signIn: (token:string) => void;
     signOut: () => void;
